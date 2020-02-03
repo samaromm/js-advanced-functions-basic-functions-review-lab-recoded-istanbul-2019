0942b10445aec352c0b651305b9e5d11f89f48f9
Samar Omar <samaromm98@gmail.com>
2020-02-03 12:40:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ function saturdayFun(s='roller-skate'){
   return "This Saturday, I want to "+s+"!"
 }
 
-function mondayWork(s='')
+function mondayWork(s='go to the office'){
+  return "This Monday, I Will "+s+"."
+}
